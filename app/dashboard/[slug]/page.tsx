4680f259d84ff71f2a225e3b0158ec2a81f4a68a
Herman Adu <herman.adu@hotmail.com>
2025-05-21 19:16:03 +0100

    project setup and next js basics

---
+++ app/dashboard/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import { Counter } from "@/app/components/counter";
+
+type Params = Promise<{ slug: string }>;
+
+export default async function DynamicSlugRoute({ params }: { params: Params }) {
+  const { slug } = await params;
+  console.log(slug);
+
+  return (
+    <>
+      <h1>Dynamic Slug Route</h1>
+      <p>/{slug}</p>
+      <Counter />
+    </>
+  );
+}
